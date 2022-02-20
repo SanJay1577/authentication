0042869ea85026b1a24eefc60be8f7d3f35b5540
Sanjay R <sanjayprashanth1997@gmail.com>
2022-02-20 19:46:44 +0530

    first commit for deploying in heroku

---
+++ db.js
@@ -0,0 +1,7 @@
+import { Db } from "mongodb";
+
+db.user.insertOne({
+    name:"Sanjay",
+    password:"password@123",
+    email:"sanjayprashanth1997@gmail.com"
+})
---
+++ index.js
@@ -0,0 +1,119 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+import {MongoClient} from 'mongodb'
+
+
+
+const MongoUrl = process.env.MongoUrl;
+
+
+async function createConnection (){
+    const client = new MongoClient(MongoUrl);
+    await client.connect();
+    console.log("Mongo is connected");
+    return client;
+}
+
+const client = await createConnection(); 
+
+async function generatePassword(password){
+    const salt = await bcrypt.genSalt(10);// number of rounds :
+    console.log(`The salt value is : ${salt}`)
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log(`The hashed password is : ${hashedPassword}`);
+    return hashedPassword;
+}
+
+async function getUserByName(name){
+    return await client.db("users").collection("user").findOne({name:name})
+}
+
+async function getUserByEmail(email){
+    return await client.db("users").collection("user").findOne({email:email})
+}
+
+
+app.get("/", async(req,res)=>{
+    res.send('Welcome to the website')
+})
+
+app.get("/users", async (req,res)=>{
+    const user = await client
+    .db("users")
+    .collection("user").find().toArray()
+    res.send(user)
+});
+
+app.delete("/users", async(req,res)=>{
+    const username = req.query
+    const deleteUser = await client
+    .db("users")
+    .collection("user")
+    .deleteMany({});
+    deleteUser ? res.send(deleteUser) : res.status(404).send({message:"No Username found"})
+})
+
+app.post("/users/signup", async (req, res) => {
+    const {name, password, email} = req.body;
+    console.log(name, password, email);
+    const isUserExist = await getUserByName(name);
+    const isEmailExist = await getUserByEmail(email)
+    console.log(isUserExist, isEmailExist);
+
+    if(isUserExist||isEmailExist){
+        res.status(400).send({message:"UserName or Email already exist"})
+        return;
+    }
+
+    if(
+        !/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#%&]).{8,}$/g.test(password)
+    )
+    {
+        res.status(400).send({message:"Password doesnot match"})
+        return;
+    }
+
+    const hashedPassword = await generatePassword(password)
+    const AddUser = await client
+      .db("users")
+      .collection("user")
+      .insertOne({name:name, password:hashedPassword, email:email});
+      res.send(AddUser);
+  });
+
+  app.post("/users/login", async (req, res) => {
+    const {name, password, email} = req.body;
+    console.log(name, password, email);
+    const userFromDb = await getUserByName(name);
+    const userEmailFromDb = await getUserByEmail(email)
+    console.log(userFromDb, userEmailFromDb);
+
+    if(!userFromDb||
+        !userEmailFromDb){
+        res.status(400).send({message:"Invalid Credentials"})
+        return;
+    }
+
+    const storedDbPassword = userFromDb.password;
+    const isPasswordMatch = await bcrypt.compare(password,storedDbPassword);
+
+    
+    if(!isPasswordMatch){
+        res.status(400).send({message:"Invalid Credentials"})
+        return;
+    }
+
+    res.send(isPasswordMatch);
+  });
+
+
+console.log("nodemon working")
+
+
+
+app.listen(PORT, ()=>console.log(`Server started in localhost:${PORT}`));
